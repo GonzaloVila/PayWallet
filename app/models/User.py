c124b69ef03939306554e9f91f0e455d4065244a
gonza <gonzalovila08@gmail.com>
2026-02-03 14:46:42 -0300

    Structure project

---
+++ app/models/User.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, id, name, password, postal_code, email, phone, age):
+        self.id = id
+        self.name = name
+        self.password = password
+        self.postal_code = postal_code
+        self.email = email
+        self.phone = phone
+        self.age = age
+
+    # ...existing methods...
